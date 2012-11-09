025902e3128c110a8f49fceb195f2523aab9ef81
Kyouhei Hoshiko <hoshiko@hoshiko-no-MacBook-Pro.local>
2012-11-09 23:47:33 +0900

    make space

--- main.js
+++ main.js
@@ -13,54 +13,76 @@ var baseThree = function(content,pass){
 	var that = Planet(content,pass);
 	var g = general();
 	var renderer,camera,scene,light,animeHandle;
-	var mesh;
+	var mesh,group;
 	var init = function(){
 		g.setTargetSize(content.id);
-		mesh = pass.makePlanet({size:60});
-		scene = new THREE.Scene();
-
-		camera = new THREE.PerspectiveCamera(40, g.winSize.x / g.winSize.y, 1, 1000);
-		camera.position.z = -400;
-		camera.lookAt(mesh.position);
-		light    = new THREE.DirectionalLight(0xffffff, 1.5);
-		light.position = {x: 0.2, y: 0.2, z: -0.2};
+		group = new THREE.Object3D();
+		mesh  = pass.makePlanet({size:40});
+		testMesh = pass.testPlanet();
+		testMesh.position.x  += 100;
+		group.add( mesh );
+		group.add( testMesh );
 		
-		scene.add(mesh);
+		
+		light = new THREE.DirectionalLight(0xffffcc, 1.4);
+		light.position = {x: 0.2, y: 0.2, z: -0.2};
+
+		scene = new THREE.Scene();
+		//scene.add(mesh);
 		scene.add(light);
-		scene.add(camera);
+		scene.add(group);
+		
+		
+		starInit();
 		
 		renderer = new THREE.WebGLRenderer({antialias: true});
-		renderer.setSize(g.winSize.x,g.winSize.y);
+		initCamera();
 		document.getElementById(content.id).appendChild(renderer.domElement);
-		renderer.render(scene, camera);
-		console.log("init");
+		//console.log("init");
 	};
 	that.init = init;
 	
-	var resize = function(){
-		var g = general();
+	var starInit = function(){
+		var material = new THREE.MeshLambertMaterial({color: 0xFFFFCC});
+		var wide = 0;
+		for ( var i = 0; i < 400; i ++ ) {
+			wide = Math.random() * 3;
+			var geometry = new THREE.SphereGeometry(wide, 10, 10);
+			var mesh = new THREE.Mesh( geometry, material );
+			mesh.position.x = 400 - Math.random() * 800;
+			mesh.position.y = 400 - Math.random() * 800;
+			mesh.position.z = 150 + Math.random() * 1000;
+			group.add( mesh );
+		}
+	};
+	var initCamera = function(){
+		g = general();
 		camera = new THREE.PerspectiveCamera(40, g.winSize.x / g.winSize.y, 1, 1000);
 		camera.position.z = -400;
 		camera.lookAt(mesh.position);
 		scene.add(camera);
 		renderer.setSize(g.winSize.x,g.winSize.y);
 		renderer.render(scene, camera);
-		//cancelAnimationFrame(animeHandle);
-
-		console.log("resize");
 	};
-	that.resize = resize;
 	
 	var animate = function(){
 		animeRotate();
 	};
 	that.animate = animate;
+	
 	var animeRotate = function(){
-		mesh.rotation.y += 0.01;
+		group.rotation.z += 0.00005;
 		renderer.render(scene, camera);
 		animeHandle = requestAnimationFrame(animeRotate);
 		//console.log("animate");
 	};
+	
+	var resize = function(){
+		initCamera();
+		//cancelAnimationFrame(animeHandle);
+		console.log("resize");
+	};
+	that.resize = resize;
 	
 	var test = function(){
 		width  = $("#canvas-wrapper").width();         
@@ -75,12 +97,25 @@ var baseThree = function(content,pass){
 var Planet = function(content,pass){
 	var pass = pass || {};
 	var makePlanet = function(args){
-		var geometry = new THREE.SphereGeometry(args.size, 20, 20);
+		var divisions = 3;
+		var geometry = new THREE.SphereGeometry(args.size, 12, 12);
+		var smooth = THREE.GeometryUtils.clone( geometry );
+			smooth.mergeVertices();
+		var modifier = new THREE.SubdivisionModifier(divisions);
+			modifier.modify( smooth );
 		var material = new THREE.MeshLambertMaterial({color: 0xF4A460});
-		mesh = new THREE.Mesh(geometry, material);
+		mesh = new THREE.Mesh(smooth, material);
 		return mesh;
 	};
 	pass.makePlanet = makePlanet;
+	
+	var testPlanet = function(){
+		var geometry = new THREE.CubeGeometry(50, 50, 50); // 立方体作成
+		var material = new THREE.MeshLambertMaterial({color: 0x0000aa}); // 材質作成
+		var mesh     = new THREE.Mesh(geometry, material);
+		return mesh;
+	};
+	pass.testPlanet = testPlanet;
 	
 	return pass;
 };
